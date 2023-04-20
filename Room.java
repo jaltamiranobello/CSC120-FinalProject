399449ea7e40efbabe9459fc3747b82332ca5257
Taylor Knibb <tknibb@smith.edu>
2023-04-19 20:12:34 -0400

    room updated

--- Room.java
+++ Room.java
@@ -1,3 +1,27 @@
+import java.util.*;
+
 public class Room {
     
+    //attributes for basic room
+    private String exit;
+    private boolean key;
+    private String type;
+    private boolean light;
+    private ArrayList<String> items;
+    
+    public Room(String exit, boolean key, String type, boolean light) {
+        this.exit = exit;
+        this.key = key;
+        this.type = type;
+        this.light = light;
+    }
+
+    public getExit() {
+        
+    }
+
+
+    public toString() {
+
+    }
 }
