295b4c41a7d7be31e188647865be614de7323859
jaltamiranobello <jaltamiranobello@smith.edu>
2023-04-20 12:00:59 -0400

    character update 1

--- Character.java
+++ Character.java
@@ -5,11 +5,13 @@ public class Character {
     private String name;
     private ArrayList<String> notes;
     private ArrayList<String> collected_items;
+    private String position;
 
-    public Character(String name){
+    public Character(String name, String position){
         this.name = name;
         this.notes = new ArrayList<String>();
         this.collected_items = new ArrayList<String>();
+        this.position = "South";
     }
 
     public ArrayList<String> getNotes(){
@@ -24,8 +26,30 @@ public class Character {
         return this.name;
     }
 
-    public void move(String direction){
-
+    public void walk(String direction){ 
+        try{
+            if (this.position == direction){
+                throw new RuntimeException(this.name + " is already here.");
+            }
+            if (direction == "North"){
+                System.out.println(this.name + " moved towards the northern wall.");
+            }
+    
+            else if (direction == "East"){
+                System.out.println(this.name + " moved towards the eastern wall.");
+            }
+    
+            else if (direction == "South"){
+                System.out.println(this.name + " moved towards the southern wall.");
+            }
+    
+            else if (direction == "West"){
+                System.out.println(this.name + " moved towards the western wall.");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
 
     public void open(){
