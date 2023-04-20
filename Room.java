70b4c7970c241958708fe04424ba847617617061
jaltamiranobello <jaltamiranobello@smith.edu>
2023-04-19 20:50:22 -0400

    updated room

--- Room.java
+++ Room.java
@@ -4,14 +4,14 @@ public class Room {
     
     //attributes for basic room
     private String exit;
-    private boolean key;
+    private String code;
     private String type;
     private boolean light;
     private ArrayList<String> items;
     
-    public Room(String exit, boolean key, String type, boolean light) {
+    public Room(String exit, String code, String type, boolean light) {
         this.exit = exit;
-        this.key = key;
+        this.code = code;
         this.type = type;
         this.light = light;
         this.items = new ArrayList<String>();
@@ -21,8 +21,8 @@ public class Room {
         return this.exit;  
     }
 
-    public boolean getKey() {
-     return this.key;   
+    public String getCode() {
+     return this.code;   
     }
 
     public String getType() {
@@ -38,7 +38,7 @@ public class Room {
     }
 
     public String toString() {
-        String x = "This is a " + this.type + ". This room has " + this.items.size() + " objects; try to find the key within the objects.";
+        String x = "This is a " + this.type + ". This room has " + this.items.size() + " objects; you can chose to retrieve items or not (be weary of your decisions). This room also has a specific code to be able to exit. Find the code by exploring the room.";
         return x;
     }
 }
