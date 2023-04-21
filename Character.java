1212e8bda3a3c64d8af263cff9825718848a939d
jaltamiranobello <jaltamiranobello@smith.edu>
2023-04-20 22:52:36 -0400

    julie character changes

--- Character.java
+++ Character.java
@@ -6,21 +6,34 @@ public class Character {
     private ArrayList<String> notes;
     private ArrayList<String> collected_items;
     private String position;
+    private ArrayList<String> held_items;
+    private Room room_location;
 
     public Character(String name, String position){
         this.name = name;
-        this.notes = new ArrayList<String>();
-        this.collected_items = new ArrayList<String>();
+        this.notes = new ArrayList<>();
+        this.collected_items = new ArrayList<>();
         this.position = "South";
+        this.held_items = new ArrayList<>();
+        this.room_location = ;
     }
 
     public ArrayList<String> getNotes(){
         return this.notes;
     }
 
-    public ArrayList<String> getcollected_items(){
+    public ArrayList<String> getCollectedItems(){
         return this.collected_items;
     }
+
+    public ArrayList<String> getHeldItems(){
+        return this.held_items;
+    }
+
+    public Room getRoomLocation(){
+        return this.room_location;
+    }
+
 
     public String getName(){
         return this.name;
@@ -51,41 +64,108 @@ public class Character {
             System.out.println(e);
         }
     }
-
-    public void open(){
-
-    }
-
-    public void close(){
+    // Hastable (object, location)
+    // Create a class for Furniture
+    public void open(String object){
 
     }
 
-    public void read(){
+    public void close(String object){
 
     }
 
-    public void write() {
+    // Create letter class
+    public void read(Letter l){
 
     }
 
+    public void write(String note){
+        this.notes.add(note);
+        System.out.println(this.name + " has written " + note + "in their notebook.");
+    }
+
+    public void viewnotes(){
+        try{
+            if (this.notes.isEmpty()){
+            throw new RuntimeException(this.name + " has no notes...");
+            }
+            for (int i = 0; i < this.notes.size(); i ++){
+                System.out.println(this.notes.get(i) + "\n");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    
     public void examine(){
 
     }
 
-    public void grab(){
+    public void grab(String object){
+        try{
+            if (this.held_items.size() == 2){
+                throw new RuntimeException(this.name + " is holding something in each hand, drop an object in order to grab another...");
+            }
+            this.held_items.add(object);
+            System.out.println(this.name + " picked up a/an" + object);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void keep(String object){
+        try{
+            if (this.collected_items.contains(object)){
+                throw new RuntimeException(this.name + " already has this...");
+            }
+            this.collected_items.add(object);
+            System.out.println(this.name + " kept the " + object);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void drop(String object){
+        try{
+            if (this.held_items.contains(object) == false){
+                throw new RuntimeException(this.name + " cannot drop " + object + "because they are not holding it...");
+            }
+            this.held_items.remove(object);
+            System.out.println(this.name + " has dropped " + object + ".");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
 
     }
 
-    public void keep(){
+    public void enterCode(Room room, String code){
+        if (code == room.getCode()){
+
+
+        }
 
     }
 
-    public void drop() {
+    public void enter(Room room){
+        try{
+            if (this.room_location.equals(room)){
+                throw new RuntimeException(this.name + " is already in this room...");
+            }
+            this.room_location = room;
+            System.out.println(this.name + " has entered " + room);
+        } catch(Exception e){
+            System.out.println(e);
+        }
 
     }
 
-    public void enterCode() {
-
+    public void exit(Room room){
+        if (room.getExit())
     }
 
 }
