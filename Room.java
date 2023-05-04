eaab8c021cabc21af5d6c084ac0ce74af0247a38
Taylor Knibb <tknibb@smith.edu>
2023-05-03 20:52:12 -0400

    changes

--- Room.java
+++ Room.java
@@ -1,4 +1,3 @@
-//import java.util.*;
 
 public class Room {
     
@@ -6,11 +5,15 @@ public class Room {
     private String code;
     public boolean light;
     
+    /** constructor for Room class
+     * @param String code: this is the code to enter the room
+     */
     public Room(String code) {
         this.code = code;
         this.light = false;
     }
     
+    /** getter for String code */
     public String getCode() {
      return this.code;   
     }
